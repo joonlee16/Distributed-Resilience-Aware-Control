ebf98996998d3aeaf6c212422265fcf1f559ea00
haejoonl <5217232@naver.com>
2025-04-20 22:55:13 -0400

    Cleaned the repo

--- simulate.py
+++ simulate.py
@@ -2,14 +2,17 @@ import cvxpy as cp
 import matplotlib.pyplot as plt
 import numpy as np
 from single_integrator import *
+from helper import *
 
+# Choose the scenario
+scenario = choose()
+
+# Sim Parameters  
 plt.ion()
 fig = plt.figure()
 ax = plt.axes(xlim=(-3.25,3.25),ylim=(-1.5,1.5)) 
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
-
-# Sim Parameters  
 F = 2
 R = 3
 d_min = 0.5
@@ -22,7 +25,7 @@ dt =0.002
 max_T = T/dt
 step_size = tau/dt
 
-#Initialize the robots
+# Initialize the robots
 robots = []
 y_offset = 0.9
 robots.append( Malicious(np.array([-1.1,y_offset - 0.5]),'#d62728',1.0, ax, F,0))
@@ -40,31 +43,18 @@ robots.append( Agent(np.array([1.3,y_offset - 0.6]),'#1f77b4',1.0 , ax, F,10))
 n =len(robots)
 F_prime = F + n // 2
 
-############################## Optimization problems ######################################
+############################## Optimization problems ###################################
 u1 = cp.Variable((2,1))
-u1_ref = cp.Parameter((2,1),value = np.zeros((2,1)) )
+u1_des = cp.Parameter((2,1),value = np.zeros((2,1)) )
 A1 = cp.Parameter((num_constraints1,2),value=np.zeros((num_constraints1,2)))
 b1 = cp.Parameter((num_constraints1,1),value=np.zeros((num_constraints1,1)))
 const1 = [A1 @ u1 >= b1]
 const1+= [u1<=umax, -umax<=u1]
-objective1 = cp.Minimize( cp.sum_squares( u1 - u1_ref  ))
+objective1 = cp.Minimize( cp.sum_squares( u1 - u1_des  ))
 cbf_controller = cp.Problem( objective1, const1 )
-###########################################################################################
+########################################################################################
 
-
-#Define the parametrized sigmoid functions
-eps = 1/(n-1)- 0.001
-q1 = 2+eps
-q2 = 0.3
-sigmoid_A1 = lambda x: q1 / (1+np.exp(-q2*x**2)) - q1/2
-def compute_der(x_i, x_j):
-    dist = (R**2 - np.sum((x_i-x_j)**2))**2
-    exp_term = 2 * q1 * q2* np.exp(-q2 *dist)
-    denominator = (1 + exp_term)**2
-    coefficient = exp_term / denominator
-    return - coefficient * ((x_i - x_j))*2*(R**2 - np.sum((x_i-x_j)**2))
-
-#Setting up the goal
+# Setting up the goal location
 goal = np.array([[0,100],[100,0]])
 
 # Start the simulation 
@@ -73,19 +63,19 @@ H =[[] for i in range(n)]
 robustness_history = []
 while True:   
     x = np.array([aa.location.reshape(1,-1)[0] for aa in robots])
-    #Compute the actual robustness
+    # Compute the actual robustness
     edges = []
     for i in range(n):
         for j in range(i+1,n):
             if np.linalg.norm(x[i]-x[j]) <=R:
                 edges.append((i,j))
 
-    #Agents form a network
+    # Agents form a network
     for (i,j) in edges:
         robots[i].connect(robots[j])
         robots[j].connect(robots[i])
 
-    #Get the nominal control input
+    # Get the nominal control input
     u_des = []
     for i in range(1,n+1):
         helper = (-1)**i * np.array([100, 0])
@@ -94,37 +84,34 @@ while True:
         vector = (helper - x[i-1]).reshape(-1,1)
         u_des.append(vector/np.linalg.norm(vector)) 
 
-    #Compute the h_i and \frac{\partial h_i/} {\partial x}
-    h = np.zeros(n)
-    der_ = np.zeros((n,n,2))
-
-    for (i,j) in edges:
-        dist = R**2-np.sum((x[i]-x[j])**2)
-        h[i]+= sigmoid_A1(dist)
-        h[j]+= sigmoid_A1(dist)
-        der_[i,i]+=compute_der(x[i],x[j])
-        der_[i,j]= -compute_der(x[i],x[j])
-        der_[j,j]+=compute_der(x[j],x[i])
-        der_[j,i]= -compute_der(x[j],x[i]) 
+    # Compute the h_i and \frac{\partial h_i/} {\partial x}
+    h, der_ = compute_h_and_der(n,x,edges,R)
     h = h-F_prime
 
     # Store the connectivity levels
     for i in range(n):
         H[i].append(h[i])
+
     #Set up the constraint of QP
     A1.value[:,:]=0
     b1.value[:,:]=0
     control_input = []
 
-    # Set up the weight w and h_hat = h
-    w = [15, 25]
+    # Set up the weight w and h_hat values according to the scenario
+    w = [15, 25]    
     h_hat = h
+    if scenario == 2:
+        h_hat[0:2]+=3.5
+    elif scenario ==3:
+        w = [17.5, 30]    
+        h_hat[0:2]-=2.5
+
     if np.any(h_hat<0):
         print(counter, h_hat)
 
     # Each robot constructs the QP and computes its contorl input u_i locally
     for i in range(n):
-        u1_ref.value = u_des[i]
+        u1_des.value = u_des[i]
         N_i = robots[i].neighbors_id()
         B_i = np.append(N_i,i)
         c = []; c_der_ = []
@@ -135,14 +122,12 @@ while True:
         c = np.array(c); c_der_ = np.array(c_der_)
         c_exp_list = np.exp(-w[1]*c).reshape((1,-1))
         c_exp_der = c_exp_list*w[1]
-
         exp_list = np.exp(-w[0]*(h_hat[B_i])).reshape((1,-1))
-
-        exp_list1 = np.exp(-w[0]*(h_hat[i]))
-
         exp_der = exp_list*w[0]
         A1.value[0,:]= exp_der @ (der_[B_i,i].reshape(-1,2)) + c_exp_der @ (c_der_.reshape(-1,2))
-        b1.value[0,0]= -alphas*(1/n-exp_list1) + alphas*(sum(c_exp_list[0]))/2
+        b1.value[0,0]= -alphas*(1/n-sum(exp_list[0])/(F_prime+1)) + alphas*(sum(c_exp_list[0]))/2
+        # b1.value[0,0]= -alphas*(1/n-exp_list1) + alphas*(sum(c_exp_list[0]))/2
+
         cbf_controller.solve(solver="GUROBI")
         if cbf_controller.status!='optimal':
             print("Error: should not have been infeasible here")
@@ -151,23 +136,26 @@ while True:
         else:
             control_input.append(u1.value)
 
-    #Agents share their values with neighbors every tau seconds
-    if counter >50 and counter% step_size ==0:
+    # Agents share their values with neighbors every tau seconds
+    if counter >50 and counter % step_size ==0:
         for aa in robots:
             aa.propagate()
         # The agents perform W-MSR
         for aa in robots:
             aa.w_msr()
-        # All the agents update their LED colors
+        # All the agents update their LED colors based on its consensus states (it is used to color the past trajectories of each agents)
         for aa in robots:
             aa.set_color()
 
-    # implement control input \mathbf u and plot the trajectory
+    # Each robot implements its own control input u_i
     for i in range(n):
         robots[i].step(control_input[i], dt) 
         robots[i].reset_neighbors()
         
-    #Plots the environment and robots
+        '''Colors the trajectories in their current LED colors'''
+        # if counter>0:
+        #     plt.plot(robots[i].locations[0][counter-1:counter+1], robots[i].locations[1][counter-1:counter+1], color = robots[i].LED, zorder=0)  
+    '''Plots the environment and robots: uncomment to visualize the simulation online. Will slow down the simulation significantly'''
     # fig.canvas.draw()
     # lines = []
     # for (i, j) in edges:
@@ -181,33 +169,30 @@ while True:
     #     ))
 
     # fig.canvas.flush_events()  
+
     # for line in lines:
     #     l = line[0]
     #     l.remove()
 
-    #If time, terminate
+    # If time, terminate
     counter+=1
-
     if counter>=max_T:
         break
-
 
 plt.ioff()
 fig2 = plt.figure()
 
-#Plot the evolutions of h_{i}'s values
+# Plot the evolutions of h_{i}'s values
 for i in range(n):
     if i <=1:
-        plt.plot(np.arange(counter), H[i],linestyle='dashdot', label="$h_{" +  str(i)+ '}$')
+        plt.plot(np.arange(counter), H[i],"r--")
     else:
-        plt.plot(np.arange(counter), H[i], label="$h_{" +  str(i)+ '}$')
-plt.plot(np.arange(counter), [0]*counter, 'k--',label="threshold", )
+        plt.plot(np.arange(counter), H[i], color="C{}".format(i % 10))
+plt.plot(np.arange(counter), [0]*counter, 'k--')
 plt.title("Evolution of $h_i$")
-plt.yticks(np.arange(-1, 4, 1.0))
 plt.show()
 
-
-#Plot the evolutions of consensus values
+# Plot the evolutions of consensus states
 length_of_consensus = len(robots[0].history)*step_size
 for aa in robots:
     temp = np.repeat(np.array(aa.history),step_size)
